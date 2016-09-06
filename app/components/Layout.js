26383854f8c35df7fb5bdecf4e21ac915609dad6
Zack Siri <zack@codemy.net>
2016-09-07 00:57:08 +0700

    refactored the layout

---
+++ app/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div id='Layout'>
+        Hello from Layout
+      </div>
+    );
+  }
+}
+
+export default Layout;
